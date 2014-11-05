623ed51e52063853441d99f6bed839a622a26452
mzmttks <ta.mizumoto@gmail.com>
2014-11-06 00:48:24 +0900

    add path

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
 from setuptools import setup
+import sys
+
+sys.path.append('./pyestat')
+sys.path.append('./test')
 
 setup(name="pyestat",
       version="0.1",
       description="Python interface of e-Stat",
-      test_suite='test_getData.suit'
+      test_suite='test_getData.suite'
 )
