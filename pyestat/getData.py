1a26249248f5474468bac96fa1da9bde0da7daad
mzmttks <ta.mizumoto@gmail.com>
2014-11-07 00:47:33 +0900

    support xml2obj

--- pyestat/getData.py
+++ pyestat/getData.py
@@ -1,28 +1,29 @@
 import os
+import lxml.etree
+
 
 def constructURL(command, params=None):
     """ construct a URL for e-stat """
     base = "http://api.e-stat.go.jp/rest/1.0/app"
-    key  = os.environ["PYESTAT_KEY"]
+    key = os.environ["PYESTAT_KEY"]
 
     commandList = {
         "getStatsList": ["surveyYears", "openYears", "statsField", "statsCode",
                          "searchWord", "searchKind", "statsNameList"],
-        "getMetaInfo" : ["statsDataId"],
+        "getMetaInfo": ["statsDataId"],
         "getStatsData": ["dataSetId", "statsDataId", "limit"]
     }
 
     # command check
-    if not command in commandList.keys():
-        raise ValueError(
-            "command must one of " + ", ".join(commandList))
+    if command not in commandList.keys():
+        raise ValueError("command must one of " + ", ".join(commandList))
 
     # argument check
     for param in params.keys():
-        if not param in commandList[command]:
+        if param not in commandList[command]:
             raise ValueError(
-                "argument of %s must one of " % (command) \
-                + ", ".join(commandList[command]))        
+                "argument of %s must one of " % (command)
+                + ", ".join(commandList[command]))
 
     # construct url
     url = "%s/%s?appId=%s" % (base, command, key)
@@ -30,8 +31,79 @@ def constructURL(command, params=None):
         for key in params.keys():
             url += "&" + key + "=" + params[key]
 
-    print url
     return url
+
+def xml2obj_sub(xml):
+    ret = {}
+    for tag in xml.iterchildren():
+        ret[tag.tag.lower()] = tag.text
+    return ret
+
+def xml2obj_attrib2dict(xml):
+    obj = {}
+    for attrib in xml.attrib.keys():
+        obj[attrib.lower()] = xml.attrib[attrib]
+    if xml.text:
+        if obj:
+            obj["text"] = xml.text
+        else:
+            obj = xml.text
+    return obj
+
+def xml2obj(string):
+    """XML response to object"""
+    xml = lxml.etree.fromstring(string)
+    obj = {}
+
+    if xml.tag == "GET_STATS_LIST":
+        obj["result"] = xml2obj_sub(xml.find("RESULT"))
+        obj["parameter"] = xml2obj_sub(xml.find("PARAMETER"))
+        obj["datalist_inf"] = []
+        for tag in xml.find("DATALIST_INF").iterfind("LIST_INF"):
+            listinf = xml2obj_attrib2dict(tag)
+            for tmp in tag.iterchildren():
+                listinf[tmp.tag.lower()] = xml2obj_attrib2dict(tmp)
+            obj["datalist_inf"].append(listinf)
+
+        return obj
+
+    if xml.tag == "GET_META_INFO":
+        obj["result"] = xml2obj_sub(xml.find("RESULT"))
+        obj["parameter"] = xml2obj_sub(xml.find("PARAMETER"))
+
+        obj["metadata_inf"] = {}
+        obj["metadata_inf"]["table_inf"] = xml2obj_sub(xml.find("METADATA_INF").find("TABLE_INF"))
+        obj["metadata_inf"]["class_inf"] = []
+        for tag in xml.find("METADATA_INF").find("CLASS_INF").iterchildren():
+            classobj = xml2obj_attrib2dict(tag)
+            classobj["class"] = [t.attrib for t in tag.iterchildren()]
+            obj["metadata_inf"]["class_inf"].append(classobj)
+        return obj
+
+    if xml.tag == "GET_STATS_DATA":
+        obj["result"] = xml2obj_sub(xml.find("RESULT"))
+        obj["parameter"] = xml2obj_sub(xml.find("PARAMETER"))
+
+        obj["statistical_data"] = {
+            "table_inf": {},
+            "class_inf": [],
+            "data_inf": {"note": [], "value": []}
+        }
+        obj["statistical_data"]["table_inf"] = xml2obj_sub(xml.find("STATISTICAL_DATA").find("TABLE_INF"))
+        for tag in xml.find("STATISTICAL_DATA").find("CLASS_INF").iterchildren():
+            classobj = xml2obj_attrib2dict(tag)
+            classobj["class"] = [t.attrib for t in tag.iterchildren()]
+            obj["statistical_data"]["class_inf"].append(classobj)
+
+        for tag in xml.find("STATISTICAL_DATA").find("DATA_INF").iterfind("NOTE"):
+            note = xml2obj_attrib2dict(tag)
+            obj["statistical_data"]["data_inf"]["note"].append(note)
+
+        for tag in xml.find("STATISTICAL_DATA").find("DATA_INF").iterfind("VALUE"):
+            value = xml2obj_attrib2dict(tag)
+            obj["statistical_data"]["data_inf"]["value"].append(value)
+
+        return obj
 
 
 def find(year=None, month=None, statsCode=None, keyword=None):
@@ -65,3 +137,4 @@ def find(year=None, month=None, statsCode=None, keyword=None):
 
 
     url = constructURL("getStatsList", params)
+
--- test/test_getData.py
+++ test/test_getData.py
@@ -1,5 +1,9 @@
 import pyestat
 import unittest
+import pprint
+
+pp = pprint.PrettyPrinter(indent=2)
+
 
 class TestGetData(unittest.TestCase):
     def setUp(self):
@@ -8,7 +12,6 @@ class TestGetData(unittest.TestCase):
     def test_constructData(self):
         url = pyestat.constructURL("getStatsList", {"surveyYears": "b"})
         self.assertTrue(url[:4] == "http")
-
 
     def test_find_date(self):
         # find stats on 2010
@@ -29,6 +32,31 @@ class TestGetData(unittest.TestCase):
 
         # find stats from 2010-1 to 2010-10
         result = pyestat.find(year=2010, month=(1, 10))
+
+    def checkKeys(self, obj, keys):
+        for k in keys:
+            self.assertTrue(k in obj.keys())
+
+    def test_xml2dataList(self):
+        obj = pyestat.xml2obj("".join(open("test/statsList.xml")))
+        self.checkKeys(obj["result"], ["status", "error_msg", "date"])
+        self.checkKeys(obj["parameter"], ["lang"])
+        self.assertIsInstance(obj["datalist_inf"], list)
+        [self.checkKeys(l, ["stat_name", "gov_org", "statistics_name", "title",
+                            "cycle", "survey_date", "open_date", "small_area"])
+         for l in obj["datalist_inf"]]
+
+    def test_xml2dataMeta(self):
+        obj = pyestat.xml2obj("".join(open("test/metaInfo.xml")))
+        self.checkKeys(obj["result"], ["status", "error_msg", "date"])
+        self.checkKeys(obj["parameter"], ["lang"])
+        self.checkKeys(obj["metadata_inf"], ["table_inf", "class_inf"])
+
+    def test_xml2dataData(self):
+        obj = pyestat.xml2obj("".join(open("test/statsData.xml")))
+        self.checkKeys(obj["result"], ["status", "error_msg", "date"])
+        self.checkKeys(obj["parameter"], ["lang"])
+
 
 def suite():
     suite = unittest.TestSuite()
